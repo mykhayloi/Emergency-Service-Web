3f920cc773d503b6cb7d5dcd95bfa4943ecdea8e
mykhayloi <79649530+mykhayloi@users.noreply.github.com>
2021-02-25 15:44:14 +0100

    Final Web Service

---
+++ MyOpenFileDialog.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Project_3
+{
+    internal class MyOpenFileDialog
+    {
+        public MyOpenFileDialog()
+        {
+        }
+
+        public static implicit operator OpenFileDialog(MyOpenFileDialog v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SelectionChangedEventArgs.cs
@@ -0,0 +1,6 @@
+﻿namespace Project_3
+{
+    internal class SelectionChangedEventArgs
+    {
+    }
+}
